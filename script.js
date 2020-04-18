40cd9d9b3118e54533b94ab34b9b84aa1fcc1cd2
Charu Praneeth <charupraneeth@gmail.com>
2020-04-18 17:45:06 +0530

    h1 and click added

--- script.js
+++ script.js
@@ -3,6 +3,12 @@
     const hourHand = document.querySelector(".hourHand");
     const display = document.querySelector(".display");
 
+    document.addEventListener('click', musicPlay);
+function musicPlay() {
+    document.getElementById('ID').play();
+    document.removeEventListener('click', musicPlay);
+}
+
 function setDate(){
     const now = new Date();
     const sec = now.getSeconds();
@@ -16,11 +22,7 @@ function setDate(){
 
     hourHand.style.transform = `rotate(${((hour/12)*360)+90}deg) scaleX(.4)`;
     
-    const text = document.createTextNode(now);
-
-    display.appendChild(text);
- 
-
-
+    const text = document.createTextNode(`${hour}hrs 7${min}mins ${sec}seconds`);
+    display.innerHTML = `${hour}hrs ${min}mins ${sec}seconds`;
 }
 setInterval(setDate,1000)
